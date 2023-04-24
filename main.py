116bce5e21d39ba462cf6639e0ad6e7da5a05463
AdomasDauda <107749872+AdomasDauda@users.noreply.github.com>
2023-04-24 11:15:45 +0100

    change error handling when loading weights

--- main.py
+++ main.py
@@ -11,8 +11,9 @@ def load_model_weights(model_name, model, chess_gui):
     print(model.summary())
     chess_gui.draw_neural_network(model)
     try:
-        model.load_weights("models/" + model_name + ".h5")
-    except IOError:
+        model.load_weights(model_name)
+    except IOError as e:
+        print(e)
         chess_gui.show_notification("Model " + model_name + " not found")
         return
     chess_gui.show_notification("Loaded model weights " + model_name)
